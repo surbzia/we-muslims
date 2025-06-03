ef2eb4cd9b3eb6ecb63e2f26dbd131750832eefe
surbzia <realsaleem.dev@gmail.com>
2025-06-04 03:16:29 +0500

    changes

--- src/Components/Yoursupport.js
+++ src/Components/Yoursupport.js
@@ -30,7 +30,7 @@ const Yoursupport = () => {
 										YOUR SUPPORT IS REALLY POWERFUL.
 									</h2>
 									<p>
-										Lorem Ipsum is simply dummy text of the printing and
+										{`Lorem Ipsum is simply dummy text of the printing and
 										typesetting industry. Lorem Ipsum has been the industry's
 										standard dummy text ever since the 1500s, when an unknown
 										printer took a galley of type and scrambled it to make a
@@ -38,7 +38,7 @@ const Yoursupport = () => {
 										printing and typesetting industry. Lorem Ipsum has been the
 										industry's standard dummy text ever since the 1500s, when an
 										unknown printer took a galley of type and scrambled it to
-										make a type specimen book.
+										make a type specimen book.`}
 									</p>
 							</div>
 						</div>
